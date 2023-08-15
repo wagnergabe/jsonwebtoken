97dcd3b388101a8ca6a2d214dff0a30d2a5556b2
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-14 23:19:07 -0500

    create express server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
