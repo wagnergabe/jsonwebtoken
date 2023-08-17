dd58b3c723c02c5a1180febc8e6a225eb14edbf9
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-16 23:46:10 -0500

    create routes, controllers

---
+++ controllers/main.js
@@ -0,0 +1,13 @@
+const login = async (req, res) => {
+    res.send('Fake Login/Register/Signup Route')
+}
+
+const dashboard = async (req,res) => {
+    const luckyNumber = Math.floor(Math.random() * 100)
+    res.status(200).json({msg: 'Hello, user', secret: `Here is  your authorized data, y our lucker numer is ${luckyNumber}` })
+}
+
+module.exports = {
+login,
+dashboard
+}
