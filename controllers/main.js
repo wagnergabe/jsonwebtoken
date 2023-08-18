241b45c7c5c30f428d392edac4e5d30f7b007237
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-17 21:57:54 -0500

    fixed other changes, Postman working

--- controllers/main.js
+++ controllers/main.js
@@ -4,7 +4,7 @@ const login = async (req, res) => {
 
 const dashboard = async (req,res) => {
     const luckyNumber = Math.floor(Math.random() * 100)
-    res.status(200).json({msg: 'Hello, user', secret: `Here is  your authorized data, y our lucker numer is ${luckyNumber}` })
+    res.status(200).json({msg: 'Hello, user', secret: `Here is  your authorized data, your lucker numer is ${luckyNumber}` })
 }
 
 module.exports = {
