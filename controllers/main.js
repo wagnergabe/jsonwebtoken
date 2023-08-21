a83e5c8f04f857b006cfa6c905864baabaae5fc4
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-20 20:37:19 -0500

    fixed scoping issue, JWT token authenticated

--- controllers/main.js
+++ controllers/main.js
@@ -2,25 +2,27 @@ const CustomAPIError= require('../errors/custom-error')
 
 
 const jwt = require('jsonwebtoken')
-const { BadRequestError } = require('../errors')
+// const { BadRequestError } = require('../errors')
 
 const login = async (req, res) => {
-    const { username, password} = req.body
+    const {username, password} = req.body
 
     if(!username || !password) {
         throw new CustomAPIError('Please provide email and password', 400)
     } 
 
+    const id = new Date().getDate()
 
-    res.send('Fake Login/Register/Signup Route')
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+      expiresIn: '30d',
+    })
+    
+    res.status(200).json({msg: 'user created', token})
 }
 
+//normally applied to database, just placeholder for now
 
-const id = new Date().getDate()
 
-const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
-  expiresIn: '30d',
-})
 
 const dashboard = async (req,res) => {
     const luckyNumber = Math.floor(Math.random() * 100)
